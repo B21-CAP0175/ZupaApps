80ef63b90450b8d3d6fef8bad3eb95c60726e68d
fauzi <muhfauziaulia@gmail.com>
2021-05-29 10:21:14 +0700

    update function

--- app/src/main/java/com/latihan/zupaapps/MainActivity.kt
+++ app/src/main/java/com/latihan/zupaapps/MainActivity.kt
@@ -1,12 +1,16 @@
 package com.latihan.zupaapps
 
-import android.app.Activity
-import android.content.Context
+import android.Manifest
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.location.*
-import android.media.MediaRecorder
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.provider.Settings
+import android.speech.RecognitionListener
+import android.speech.RecognizerIntent
+import android.speech.SpeechRecognizer
 import android.util.Log
 import android.view.MotionEvent
 import android.view.View
@@ -18,16 +22,15 @@ import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import androidx.cardview.widget.CardView
 import androidx.core.app.ActivityCompat
-import com.google.android.gms.maps.GoogleMap
-import com.google.android.gms.maps.SupportMapFragment
+import androidx.core.content.ContextCompat
 import java.util.*
-import java.util.jar.Manifest
 
-class MainActivity : AppCompatActivity(), View.OnClickListener, OnTouchListener {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
     private lateinit var profilePic: ImageView
     private lateinit var btnSOS: Button
-    private lateinit var recordNotif: ImageView
+    private lateinit var recordNotif: CardView
     private lateinit var formButton: Button
     private lateinit var historyButton: Button
     private lateinit var logoutButton: Button
@@ -36,6 +39,8 @@ class MainActivity : AppCompatActivity(), View.OnClickListener, OnTouchListener 
 
     private var locationManager: LocationManager? = null
 
+    private lateinit var textRecord: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -43,11 +48,10 @@ class MainActivity : AppCompatActivity(), View.OnClickListener, OnTouchListener 
         profilePic = findViewById(R.id.profile_pic)
         profilePic.setOnClickListener(this)
 
-        recordNotif = findViewById(R.id.nav_bicara)
+        recordNotif = findViewById(R.id.cardView_sos)
         recordNotif.visibility = View.GONE
 
         btnSOS = findViewById(R.id.btnSOS)
-        btnSOS.setOnTouchListener(this)
 
         formButton = findViewById(R.id.btn_form)
         formButton.setOnClickListener(this)
@@ -61,6 +65,8 @@ class MainActivity : AppCompatActivity(), View.OnClickListener, OnTouchListener 
         textLocation = findViewById(R.id.textView_location)
         textLocation.setOnClickListener(this)
 
+        textRecord = findViewById(R.id.hasil_record)
+
         locationManager = getSystemService(LOCATION_SERVICE) as LocationManager?
 
         if (ActivityCompat.checkSelfPermission(this,android.Manifest.permission.ACCESS_COARSE_LOCATION)!=PackageManager.PERMISSION_GRANTED
@@ -68,36 +74,71 @@ class MainActivity : AppCompatActivity(), View.OnClickListener, OnTouchListener 
             ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.ACCESS_COARSE_LOCATION,android.Manifest.permission.ACCESS_FINE_LOCATION),111)
         }
 
-    }
-
-
-    override fun onTouch(v: View, event: MotionEvent): Boolean {
-        when (event.action) {
-            MotionEvent.ACTION_DOWN -> {
-                val anim = AnimationUtils.loadAnimation(this, R.anim.fade_in)
-                recordNotif.startAnimation(anim)
-                recordNotif.visibility = View.VISIBLE
-//                startRecord()
-                return true
-            }
-            MotionEvent.ACTION_UP -> {
-                val anim = AnimationUtils.loadAnimation(this, R.anim.fade_out)
-                recordNotif.startAnimation(anim)
-                recordNotif.visibility = View.GONE
-//                stopRecord()
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED) {
+                val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS, Uri.parse("package:" + packageName))
+                startActivity(intent)
+                finish()
+                Toast.makeText(this, "Enable Microphone Permission..!!", Toast.LENGTH_SHORT).show()
             }
         }
-        return false
+
+        speechToText()
+
     }
 
-    private fun stopRecord() {
-    }
+    private fun speechToText(){
+        val speechRecognizer = SpeechRecognizer.createSpeechRecognizer(this)
+        val speechRecognizerIntent = Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH)
+        speechRecognizerIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM)
+        speechRecognizerIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, Locale.getDefault())
 
-    private fun getFilename() {
-    }
+        speechRecognizer.setRecognitionListener(object : RecognitionListener{
+            override fun onReadyForSpeech(params: Bundle?) {}
 
-    private fun startRecord() {
-        TODO("Not yet implemented")
+            override fun onBeginningOfSpeech() {}
+
+            override fun onRmsChanged(rmsdB: Float) {}
+
+            override fun onBufferReceived(buffer: ByteArray?) {}
+
+            override fun onEndOfSpeech() {}
+
+            override fun onError(error: Int) {}
+
+            override fun onResults(results: Bundle) {
+                val matches = results.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION)
+                if (matches != null){
+                    textRecord.setText(matches[0])
+                }
+            }
+
+            override fun onPartialResults(partialResults: Bundle?) {}
+
+            override fun onEvent(eventType: Int, params: Bundle?) {}
+        })
+
+        btnSOS.setOnTouchListener(View.OnTouchListener { view, motionEvent ->
+            when (motionEvent.action) {
+                MotionEvent.ACTION_UP -> {
+                    val anim = AnimationUtils.loadAnimation(this, R.anim.fade_out)
+                    recordNotif.startAnimation(anim)
+                    recordNotif.visibility = View.GONE
+                    speechRecognizer.stopListening()
+                    textRecord.hint = "Hasil rekaman akan muncul disini"
+                }
+
+                MotionEvent.ACTION_DOWN -> {
+                    val anim = AnimationUtils.loadAnimation(this, R.anim.fade_in)
+                    recordNotif.startAnimation(anim)
+                    recordNotif.visibility = View.VISIBLE
+                    speechRecognizer.startListening(speechRecognizerIntent)
+                    textRecord.setText("")
+                    textRecord.hint = "Listening..."
+                }
+            }
+            false
+        })
     }
 
     private fun locationListener() = object :LocationListener{
@@ -108,7 +149,6 @@ class MainActivity : AppCompatActivity(), View.OnClickListener, OnTouchListener 
             textLocation.text = (""+address)
             Toast.makeText(this@MainActivity, "Location Updated", Toast.LENGTH_SHORT).show()
         }
-
     }
 
     override fun onClick(v: View){
